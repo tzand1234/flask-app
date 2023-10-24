03019ce8bb49ec9f3a807b7e09a659d2bf37182f
Tom Zandbergen <541897@student.saxion.nl>
2023-10-24 12:29:36 +0200

    test

--- flask-app/app.py
+++ flask-app/app.py
@@ -177,7 +177,7 @@ with app.app_context():
 
 if __name__ == '__main__':
     # Run the Flask application
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 # ----------------------------------------------------------------------------------------
 
 
